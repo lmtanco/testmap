6b5627c5a403e80d2b43686c042d7219395dd5e3
Luis Molina-Tanco <lmtanco@uma.es>
2015-12-02 00:20:02 +0100

    Initial Commit

---
+++ testmap/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  testmap
+//
+//  Created by Diana UMA on 2/12/15.
+//  Copyright © 2015 diana. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
